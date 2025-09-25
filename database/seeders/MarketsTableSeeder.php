af7b28c1d19517fdd294caaed3a782c56171d93e
evilboss <jr.evilboss@gmail.com>
2025-09-25 22:27:42 +0800

    add new seaders

---
+++ database/seeders/MarketsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MarketsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
