f30732ebc7aad6983ead4a398add51980f7816d9
evilboss <jr.evilboss@gmail.com>
2025-09-25 22:08:48 +0800

    sync

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::factory(10)->create();
+
         User::factory()->create([
-            'name' => 'admin user',
-            'email' => 'admin@example.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
             'password' => bcrypt('password'),
         ]);
     }
